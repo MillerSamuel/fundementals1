8c8832aee0956ed7d55c4cec9dfc92816db1e7a4
Samuel Miller <smillercb@gmail.com>
2022-06-06 12:27:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// 1
+for (int i=1;i<=255;i++){
+    Console.WriteLine(i);
+}
+
+// 2
+for (int i=1;i<=100;i++){
+    if(i%3==0&&i%5==0){
+        continue;
+    }
+    else if(i%3==0||i%5==0){
+        Console.WriteLine(i);
+    }
+}
+
+// 3
+for (int i=1;i<=100;i++){
+    if(i%3==0&&i%5==0){
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i%3==0){
+        Console.WriteLine("Fizz");
+    }
+    else if(i%5==0){
+        Console.WriteLine("Buzz");
+    }
+}
